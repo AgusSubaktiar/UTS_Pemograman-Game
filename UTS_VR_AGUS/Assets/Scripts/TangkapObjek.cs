6f3c6aee60cbfead4441fa8eaa7da0a6aed45d55
AgusSubaktiar <43577605+AgusSubaktiar@users.noreply.github.com>
2020-10-21 10:29:53 +0700

    UTS Selesai

---
+++ UTS_VR_AGUS/Assets/Scripts/TangkapObjek.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TangkapObjek : MonoBehaviour
+{
+    public GameObject bola;
+    public GameObject tangan;
+
+    bool tertangkap = false;
+
+    Vector3 posisiBola;
+    // Start is called before the first frame update
+    void Start()
+    {
+        posisiBola = bola.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (!tertangkap)
+            {
+                bola.transform.SetParent(tangan.transform);
+                bola.transform.localPosition = new Vector3(0f, -0.100f, 1.48f);   
+                tertangkap = true;
+                bola.GetComponent<Renderer>().material.color = Color.blue;
+                TextMesh hitam = GameObject.Find("Text").GetComponent<TextMesh>();
+                hitam.text = "Bola ditangkap";
+            }
+            else if (tertangkap)
+            {
+                TextMesh hitam = GameObject.Find("Text").GetComponent<TextMesh>();
+                hitam.text = "Bola dilepas";
+                bola.transform.SetParent(null);
+                bola.transform.localPosition = posisiBola;
+                tertangkap = false;
+                bola.GetComponent<Renderer>().material.color = Color.red;
+            }
+            
+        }
+    }
+}
